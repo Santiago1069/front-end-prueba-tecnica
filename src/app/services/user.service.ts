2d513843c1f2dfc693643c42c3aec95be264f8c0
Santiago Sandoval <casaba3131@gmail.com>
2022-11-01 14:28:00 -0600

    comments are added

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,27 +8,32 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
-  // TODO: poner esto en vaaribale de entorno
   API_URL = 'http://localhost:8015/api';
 
   constructor(private http: HttpClient) {}
 
+
+  //a GET request is created to communicate with the back-en to get all the users from the database
   getUsers() : Observable<Array<User>>{
     return this.http.get<Array<User>>(`${this.API_URL}/users`);
   }
 
+  //a GET request is created to communicate with the back-en to obtain a user by id from the database
   getUser(id: String) : Observable<User> {
     return this.http.get<User>(`${this.API_URL}/users/${id}`);
   }
 
+  //a DELETE request is created to communicate with the back-en to remove a user by id from the database
   deleteUser(id: String){
     return this.http.delete(`${this.API_URL}/users/${id}`);
   }
 
+  //a POST request is created to communicate with the back-en to save a user in the database
   saveUser(user: User){
     return this.http.post(`${this.API_URL}/users`, user);
   }
 
+  //a PUT request is created to communicate with the back-en to edit a user by id in the database
   updateUser(id: String, updateUser: User){
     return this.http.put(`${this.API_URL}/users/${id}`, updateUser);
 
